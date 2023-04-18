dba02a4099938581baa14c34ee3c9541e571643f
vitaly donkoglo <vitaly.donkoglo@gmail.com>
2023-04-18 17:57:46 +0300

    Write index.ts
    This is already a fully functional version

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export let env = (varname: string) => {
+  let value = process.env[varname]
+
+  if (value === undefined) {
+    console.error(`Environment variable ${varname} is not set`)
+    process.exit(1)
+  }
+
+  return value
+}
