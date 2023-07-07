098a5c0300ad1ad767f69496ffa86258fa3554b9
vitaly donkoglo <vitaly.donkoglo@gmail.com>
2023-07-07 03:45:30 +0300

    Parse .env
    I added a side effect that parses variables from the .env file and sets them. If there is no file or variables, it does nothing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+let envFile: string
+try {
+  envFile = readFileSync(resolve(process.cwd(), '.env'), 'utf8')
+  for (let str of envFile.split('\n')) {
+    let isComment = /^\s*#/.test(str)
+    if (isComment) continue
+    let match = /^\s*([^=]+?)\s*=\s*"(.*?)"/.exec(str)
+    if (match === null) continue
+    let [, key, value] = match
+    console.log(key, value)
+    process.env[key] = value
+  }
+} catch (error) {}
+
 export let env = (varname: string, fallback?: string): string => {
   let value = process.env[varname] ?? fallback
   if (value === undefined)
